4164dfc10d0239ce2cb90f8505626cc8bbf87d85
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-08-14 03:59:13 -0400

    Added ancillary::flat_set alias as well as entity_compare

---
+++ include/ecfw/entity/group.hpp
@@ -0,0 +1,19 @@
+#pragma once
+
+#include "traits.hpp"
+#include <ancillary/container/flat_set.hpp>
+
+namespace ecfw {
+
+	template <class Entity>
+	struct entity_compare {
+		bool operator()(Entity lhs, Entity rhs) const {
+			using traits_type = entity_traits<Entity>;
+			return traits_type::index(lhs) < traits_type::index(rhs);
+		}
+	};
+
+	template <class Entity>
+	using entity_group = ancillary::flat_set<Entity, entity_compare<Entity>>;
+
+}
