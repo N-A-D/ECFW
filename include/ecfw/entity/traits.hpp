e0bc439bab1c5f5d33e06652287d6a16dc4f1c2e
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-08-14 21:10:44 -0400

    Revised entity_traits to have type aliases for indexes and versions

--- include/ecfw/entity/traits.hpp
+++ include/ecfw/entity/traits.hpp
@@ -11,22 +11,26 @@ namespace ecfw {
 	template <>
 	struct entity_traits<uint64_t>
 	{
-		static constexpr uint32_t max_index = 0xFFFFFFFF;
-		static constexpr uint32_t max_version = 0xFFFFFFFF;
+		using entity_type  = uint64_t;
+		using index_type   = uint32_t;
+		using version_type = uint32_t;
 
-		static constexpr uint32_t index(uint64_t e) noexcept {
+		static constexpr index_type max_index = 0xFFFFFFFF;
+		static constexpr version_type max_version = 0xFFFFFFFF;
+
+		static constexpr index_type index(entity_type e) noexcept {
 			return e & max_index;
 		}
 
-		static constexpr uint32_t version(uint64_t e) noexcept {
+		static constexpr version_type version(entity_type e) noexcept {
 			return e >> 32;
 		}
 
-		static constexpr uint64_t create(uint32_t i, uint32_t v) noexcept {
-			return uint64_t(i) | uint64_t(v) << 32;
+		static constexpr entity_type create(index_type i, version_type  v) noexcept {
+			return entity_type(i) | entity_type(v) << 32;
 		}
 
-		static constexpr uint64_t recycle(uint64_t e) noexcept {
+		static constexpr entity_type recycle(entity_type e) noexcept {
 			assert(version(e) < max_version);
 			return create(index(e), version(e) + 1);
 		}
@@ -36,23 +40,27 @@ namespace ecfw {
 	template <>
 	struct entity_traits<uint32_t>
 	{
-		static constexpr uint32_t max_index = 0xFFFFF;
-		static constexpr uint32_t max_version = 0xFFF;
+		using entity_type  = uint32_t;
+		using index_type   = uint32_t;
+		using version_type = uint32_t;
 
-		static constexpr uint32_t index(uint32_t e) noexcept {
+		static constexpr index_type  max_index = 0xFFFFF;
+		static constexpr version_type max_version = 0xFFF;
+
+		static constexpr index_type index(entity_type e) noexcept {
 			return e & max_index;
 		}
 
-		static constexpr uint32_t version(uint32_t e) noexcept {
+		static constexpr version_type version(entity_type e) noexcept {
 			return e >> 20;
 		}
 
-		static constexpr uint32_t create(uint32_t i, uint32_t v) noexcept {
+		static constexpr entity_type create(index_type i, version_type v) noexcept {
 			assert(i <= max_index && v <= max_version);
-			return uint32_t(i) | uint32_t(v) << 20;
+			return entity_type(i) | entity_type(v) << 20;
 		}
 
-		static constexpr uint32_t recycle(uint32_t e) noexcept {
+		static constexpr entity_type recycle(entity_type e) noexcept {
 			assert(version(e) < max_version);
 			return create(index(e), version(e) + 1);
 		}
@@ -62,27 +70,31 @@ namespace ecfw {
 	template <>
 	struct entity_traits<uint16_t>
 	{
-		static constexpr uint16_t max_index = 0xFFF;
-		static constexpr uint16_t max_version = 0xF;
 
-		static constexpr uint16_t index(uint16_t e) noexcept {
+		using entity_type  = uint16_t;
+		using index_type   = uint16_t;
+		using version_type = uint16_t;
+
+		static constexpr index_type max_index = 0xFFF;
+		static constexpr version_type max_version = 0xF;
+
+		static constexpr index_type index(entity_type e) noexcept {
 			return e & max_index;
 		}
 
-		static constexpr uint16_t version(uint16_t e) noexcept {
+		static constexpr version_type version(entity_type e) noexcept {
 			return e >> 12;
 		}
 
-		static constexpr uint16_t create(uint16_t i, uint16_t v) noexcept {
+		static constexpr entity_type create(index_type i, version_type v) noexcept {
 			assert(i <= max_index && v <= max_version);
-			return uint16_t(i) | uint16_t(v) << 12;
+			return entity_type(i) | entity_type(v) << 12;
 		}
 
-		static constexpr uint16_t recycle(uint16_t e) noexcept {
+		static constexpr entity_type recycle(entity_type e) noexcept {
 			assert(version(e) < max_version);
 			return create(index(e), version(e) + 1);
 		}
 
 	};
-
 }
