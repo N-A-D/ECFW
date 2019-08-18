402df490a7186f2c73ecec7b1436fcbdb5eb20b9
Austin Datiles <ned.austin.datiles@ryerson.ca>
2019-08-18 15:52:33 -0400

    Revised tests

--- test/ecfw/default_storage.cpp
+++ test/ecfw/default_storage.cpp
@@ -1,4 +1,6 @@
 #include <gtest/gtest.h>
+#include <algorithm>
+#include <random>
 #include <ecfw/component/storage.hpp>
 
 struct Boolean {
@@ -8,21 +10,41 @@ struct Boolean {
 	bool bit;
 };
 
+constexpr size_t N = 100;
+
 TEST(DefaultStorageTests, CreationTests) {
 	ecfw::default_storage<Boolean> storage;
-	ASSERT_TRUE(storage.construct(10, true));
-	ASSERT_EQ(storage.size(), 11);
-	ASSERT_TRUE(storage.construct(5, true));
+	for (size_t i = 0; i != N; ++i) {
+		ASSERT_TRUE(storage.construct(i, true));
+	}
+	storage.clear();
+	ASSERT_TRUE(storage.empty());
+	std::vector<size_t> indices(N);
+	std::generate(indices.begin(), indices.end(), [n = 0]() mutable { return n++; });
+	std::shuffle(indices.begin(), indices.end(), std::mt19937{ std::random_device{}() });
+	for (auto index : indices) {
+		ASSERT_TRUE(storage.construct(index, true));
+	}
 }
 
 TEST(DefaultStorageTests, RetrievalTests) {
 	ecfw::default_storage<Boolean> storage;
-	ASSERT_TRUE(storage.construct(10, true));
-	ASSERT_EQ(storage.size(), 11);
-	ASSERT_TRUE(storage.construct(5, true));
-	ASSERT_FALSE(storage.get(0));
-	ASSERT_FALSE(storage.get(1));
-	ASSERT_FALSE(storage.get(2));
-	ASSERT_TRUE(storage.get(5));
-	ASSERT_TRUE(storage.get(10));
+	for (size_t i = 0; i != N; ++i) {
+		ASSERT_TRUE(storage.construct(i, true));
+	}
+	for (size_t i = 0; i != N; ++i) {
+		ASSERT_TRUE(storage.get(i));
+	}
+	storage.clear();
+	ASSERT_TRUE(storage.empty());
+	std::vector<size_t> indices(N);
+	std::generate(indices.begin(), indices.end(), [n = 0]() mutable { return n++; });
+	std::shuffle(indices.begin(), indices.end(), std::mt19937{ std::random_device{}() });
+	for (auto index : indices) {
+		ASSERT_TRUE(storage.construct(index, true));
+	}
+	std::sort(indices.begin(), indices.end());
+	for (auto index : indices) {
+		ASSERT_TRUE(storage.get(index));
+	}
 }
