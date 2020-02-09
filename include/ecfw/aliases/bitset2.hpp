67599d5559bb390526b93a4a0efdcc732cd754b6
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-02-09 11:55:57 -0500

    Moved things around

--- include/ecfw/aliases/bitset2.hpp
+++ include/ecfw/aliases/bitset2.hpp
@@ -4,8 +4,5 @@
 #include <bitset2/bitset2.hpp>
 
 namespace ecfw {
-	namespace detail {
-		template <std::size_t N>
-		using bitset = Bitset2::bitset2<N>;
-	}
+	namespace bs2 = Bitset2;
 }
---
+++ include/ecfw/fwd/bitset.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include <cstddef>
+#include <ecfw/aliases/bitset2.hpp>
+
+namespace ecfw {
+	namespace detail {
+
+		template <std::size_t N>
+		using bitset = bs2::bitset2<N>;
+
+	}
+}
