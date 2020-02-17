37a54c6022b97b522e8953dd6efaf0b197ba0327
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-02-16 19:35:44 -0500

    Updating comment

--- include/ecfw/fwd/vector.hpp
+++ include/ecfw/fwd/vector.hpp
@@ -9,9 +9,12 @@ namespace ecfw {
 		 * The purpose of this class is to serve as the default storage
 		 * implementation for components.
 		 * 
+		 * Leaves uninitialized gaps within the buffer for components not
+		 * in use.
+		 *
 		 * @note Provides O(1) lookup. Amortized O(1) insert + delete.
 		 * 
-		 * @tparam T 
+		 * @tparam T A component type.
 		 */
 		template <typename T>
 		class vector;
