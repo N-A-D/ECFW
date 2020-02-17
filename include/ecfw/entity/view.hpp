865b51c065c71cde34876760f9a7876e8902a7a2
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-02-17 16:25:11 -0500

    Reverting delete

---
+++ include/ecfw/entity/view.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <functional>
+#include <ecfw/fwd/view.hpp>
+#include <ecfw/fwd/world.hpp>
+#include <ecfw/utility/sparse_set.hpp>
+
+namespace ecfw {
+
+	template <typename C, typename... Cs>
+	class view final {
+	public:
+	private:
+	};
+
+}
