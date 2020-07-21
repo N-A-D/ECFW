fd6305bb192409f097ba59058d931cc0b839d7b0
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-07-21 00:02:11 -0400

    Removing old implementation

--- include/ecfw/aliases/bitset2.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <bitset2/bitset2.hpp>
-
-namespace ecfw { namespace detail {
-
-	namespace bs2 = Bitset2;
-
-} }
--- include/ecfw/aliases/hana.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <boost/hana.hpp>
-
-namespace ecfw { namespace detail {
-
-	namespace hana = boost::hana;
-
-} }
