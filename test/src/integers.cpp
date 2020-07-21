afd8b1788dab327dcc961da742cfc91a9c3a9a5c
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-07-21 00:05:17 -0400

    Renaming entity.hpp and functions.

---
+++ test/src/integers.cpp
@@ -0,0 +1,16 @@
+#include <random>
+#include <gtest/gtest.h>
+#include <ecfw/detail/integers.hpp>
+
+TEST(integers, packing_and_unpacking)
+{
+	namespace dtl = ecfw::detail;
+
+	std::mt19937 gen(std::random_device{}());
+	std::uniform_int_distribution<uint32_t> dist(0, 0xFFFFFFFF);
+	uint32_t msw = dist(gen);
+	uint32_t lsw = dist(gen);
+	uint64_t word = dtl::concat(msw, lsw);
+	ASSERT_EQ(msw, dtl::msw(word));
+	ASSERT_EQ(lsw, dtl::lsw(word));
+}
