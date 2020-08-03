b447ef94b1bc3dd333deb2231cd7a2ed0cc861d0
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-08-03 14:28:21 -0400

    Renaming test to reflect what is really being tested. Removing the portion of the test which compares indices.

--- test/src/type_index.cpp
+++ test/src/type_index.cpp
@@ -1,15 +1,13 @@
 #include <gtest/gtest.h>
 #include <ecfw/detail/type_index.hpp>
 
-TEST(type_index, type_index)
-{
+struct T0 {};
+struct T1 {};
+struct T2 {};
+
+TEST(type_index, generation_order) {
 	namespace dtl = ecfw::detail;
-
-	struct T0 {};
-	struct T1 {};
-	struct T2 {};
-
-	ASSERT_EQ(dtl::type_index_v<T0>, dtl::type_index_v<T0>);
-	ASSERT_LT(dtl::type_index_v<T0>, dtl::type_index_v<T1>);
-	ASSERT_GT(dtl::type_index_v<T2>, dtl::type_index_v<T1>);
+	ASSERT_EQ(dtl::type_index_v<T0>, 0);
+	ASSERT_EQ(dtl::type_index_v<T1>, 1);
+	ASSERT_EQ(dtl::type_index_v<T2>, 2);
 }
