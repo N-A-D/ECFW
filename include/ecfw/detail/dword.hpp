299848c80a408e9fce0f0c23538cbf1efc2115ec
Austin Datiles <ned.austin.datiles@ryerson.ca>
2020-09-04 19:08:39 -0400

    add nodiscard attribute

--- include/ecfw/detail/dword.hpp
+++ include/ecfw/detail/dword.hpp
@@ -7,15 +7,15 @@ namespace ecfw
 namespace detail
 {
 
-	constexpr uint64_t concat(uint32_t msw, uint32_t lsw) noexcept {
+	[[nodiscard]] constexpr uint64_t concat(uint32_t msw, uint32_t lsw) noexcept {
 		return uint64_t(msw) << 32 | uint64_t(lsw);
 	}
 
-	constexpr uint32_t msw(uint64_t dword) noexcept {
+	[[nodiscard]] constexpr uint32_t msw(uint64_t dword) noexcept {
 		return static_cast<uint32_t>(dword >> 32);
 	}
 
-	constexpr uint32_t lsw(uint64_t dword) noexcept {
+	[[nodiscard]] constexpr uint32_t lsw(uint64_t dword) noexcept {
 		return static_cast<uint32_t>(dword & 0xFFFFFFFF);
 	}
 
