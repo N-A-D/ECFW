7002be15548ea9353d7cff0d2c125a2ee6a299e1
Ned Datiles <ned.datiles@gmail.com>
2020-10-17 14:13:08 -0400

    Add tests for is_[copy/move]able.

--- test/src/type_traits.cpp
+++ test/src/type_traits.cpp
@@ -10,3 +10,36 @@ TEST(type_traits, is_iterator)
 	ASSERT_TRUE(dtl::is_iterator_v<std::vector<int>::iterator>);
 	ASSERT_FALSE(dtl::is_iterator_v<iterator>);
 }
+
+TEST(type_traits, is_copyable)
+{
+	struct copyable {};
+	struct only_copyable {
+		only_copyable(const only_copyable&) = default;
+		only_copyable& operator=(const only_copyable&) = default;
+	};
+	struct noncopyable {
+		noncopyable(const noncopyable&) = delete;
+		noncopyable& operator=(const noncopyable&) = delete;
+	};
+
+	ASSERT_TRUE(dtl::is_copyable_v<copyable>);
+	ASSERT_TRUE(dtl::is_copyable_v<only_copyable>);
+	ASSERT_FALSE(dtl::is_copyable_v<noncopyable>);
+}	
+
+TEST(type_traits, is_movable)
+{
+	struct movable {};
+	struct only_movable {
+		only_movable(only_movable&&) = default;
+		only_movable& operator=(only_movable&&) = default;
+	};
+	struct nonmovable {
+		nonmovable(nonmovable&&) = delete;
+		nonmovable& operator=(nonmovable&&) = delete;
+	};
+	ASSERT_TRUE(dtl::is_movable_v<movable>);
+	ASSERT_TRUE(dtl::is_movable_v<only_movable>);
+	ASSERT_FALSE(dtl::is_movable_v<nonmovable>);	
+}
