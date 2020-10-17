bafa8f93fc88a5f5a9a67672d7f99dada63327ed
Ned Datiles <ned.datiles@gmail.com>
2020-10-17 13:50:16 -0400

    Add traits for copyability and movability.

--- include/ecfw/detail/type_traits.hpp
+++ include/ecfw/detail/type_traits.hpp
@@ -19,5 +19,31 @@ namespace detail
 	template <typename T>
 	constexpr auto is_iterator_v = is_iterator<T>::value;
 
+	// Using these until I'm able to use C++20
+
+	template <typename T>
+	struct is_copyable : 
+		std::conjunction<
+			std::is_copy_constructible<T>, 
+			std::is_copy_assignable<T>,
+			std::is_swappable<T>
+		> 
+	{};
+
+	template <typename T>
+	constexpr auto is_copyable_v = is_copyable<T>::value;
+
+	template <typename T>
+	struct is_movable : 
+		std::conjunction<
+			std::is_move_constructible<T>,
+			std::is_move_assignable<T>,
+			std::is_swappable<T>
+		>
+	{};
+
+	template <typename T>
+	constexpr auto is_movable_v = is_movable<T>::value;
+
 } // namespace detail
 } // namespace ecfw
